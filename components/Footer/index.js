895f2600c2c4a5aa7c15e55a77e5bdc1c8c5fdaf
aritroCoder <ogcodinggod@gmail.com>
2022-12-22 01:45:55 +0530

    updated links

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,9 +5,9 @@ const Footer = () => {
     return (
         <div className={styles.footer}>
             <div>
-                Made with ❤️ by <a href="https://www.github.com/aritroCoder">aritroCoder</a>{' '}
+                Made with ❤️ by <a href="https://www.github.com/aritroCoder" target="_blank">aritroCoder</a>{' '}
             </div>
-            <a href="https://www.github.com/aritroCoder/ConfessionDapp-frontend">
+            <a href="https://www.github.com/aritroCoder/ConfessionDapp-frontend" target="_blank">
                 <Image src="/github-logo.png" width={25} height={25}></Image>
             </a>
         </div>
