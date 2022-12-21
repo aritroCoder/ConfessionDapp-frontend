df39f6248174e44580700f4c155b48bb78351921
aritroCoder <ogcodinggod@gmail.com>
2022-12-22 01:44:13 +0530

    added links and title

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,9 +5,9 @@ const Footer = () => {
     return (
         <div className={styles.footer}>
             <div>
-                Made with ❤️ by <a href="http://github.com/aritroCoder">aritroCoder</a>{' '}
+                Made with ❤️ by <a href="https://www.github.com/aritroCoder">aritroCoder</a>{' '}
             </div>
-            <a href="">
+            <a href="https://www.github.com/aritroCoder/ConfessionDapp-frontend">
                 <Image src="/github-logo.png" width={25} height={25}></Image>
             </a>
         </div>
